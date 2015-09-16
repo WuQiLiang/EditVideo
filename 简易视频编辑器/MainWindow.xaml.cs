e83f31be44a2d32384089e60e97b36bb44da8b15
WuQiLiang <2276845646@qq.com>
2015-09-16 22:53:34 +0800

    1.完成快进、后退 2.修改细节（播放暂停、视频显示标题） 3.获取视频时长

--- 简易视频编辑器/MainWindow.xaml.cs
+++ 简易视频编辑器/MainWindow.xaml.cs
@@ -24,19 +24,87 @@ namespace 简易视频编辑器
             InitializeComponent();
         }
 
+
+        /// <summary>
+        /// 视频开始与暂停
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            MediaElement1.Play();
+            
+            if (button.Content.ToString()=="播放")
+            {
+                
+                MediaElement1.Play();
+                button.Content = "暂停";
+                getVideoName();
+            }
+            else
+            {
+                MediaElement1.Pause();
+                button.Content = "播放";
+                getVideoName();
+            }
+            
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// 获取视频标题
+        /// </summary>
+        private void getVideoName()
         {
-            MediaElement1.Pause();
+            string path = this.MediaElement1.Source.LocalPath;
+            string currentfileName = path.Substring(path.LastIndexOf('\\') + 1);
+            MediaElement1.ToolTip = currentfileName;
         }
 
+
+        /// <summary>
+        /// 视频停止（停止后播放按钮重置）
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
             MediaElement1.Stop();
+            button.Content = "播放";
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            //MediaElement1.Source = new Uri(filepath, UriKind.Relative);
+        }
+
+        /// <summary>
+        /// 前进5秒
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void forward_Click(object sender, RoutedEventArgs e)
+        {
+            MediaElement1.Position = MediaElement1.Position + TimeSpan.FromSeconds(5);
+        }
+
+        /// <summary>
+        /// 视频后退
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void back_Click(object sender, RoutedEventArgs e)
+        {
+            MediaElement1.Position = MediaElement1.Position - TimeSpan.FromSeconds(5);
+        }
+
+        /// <summary>
+        /// 视频开始时获取视频时间
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MediaElement1_MediaOpened(object sender, RoutedEventArgs e)
+        {
+            totalTime.Content = MediaElement1.NaturalDuration;
+            
         }
 
     }
